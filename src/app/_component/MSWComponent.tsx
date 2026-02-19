27e11cbcb5a1355cca49553d69de068a89a0715f
hyeone999 <caps026@naver.com>
2026-02-19 12:55:12 +0900

    Fix: module error

--- src/app/_component/MSWComponent.tsx
+++ src/app/_component/MSWComponent.tsx
@@ -6,31 +6,30 @@ import { handlers } from '@/src/mocks/handlers'
 const mockingEnabledPromise =
   typeof window !== 'undefined'
     ? import('@/src/mocks/browser').then(async ({ default: worker }) => {
-      if (process.env.NODE_ENV === 'production') {
-        return;
-      }
-      await worker.start({
-        onUnhandledRequest(request, print) {
-          if (request.url.includes('_next')) {
-            return
-          }
-          print.warning()
-        },
+        if (process.env.NODE_ENV === 'production') {
+          return
+        }
+        
+        await worker.start({
+          onUnhandledRequest(request, print) {
+            if (request.url.includes('_next')) {
+              return
+            }
+            print.warning()
+          },
+        })
+        
+        worker.use(...handlers)
+        
+        console.log('MSW Handlers:', worker.listHandlers())
       })
-      worker.use(...handlers);
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (module as any).hot?.dispose(() => { worker.stop(); });
-      console.log(worker.listHandlers())
-    })
     : Promise.resolve()
 
 export function MSWProvider({
-                              children,
-                            }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode
 }>) {
-  // If MSW is enabled, we need to wait for the worker to start,
-  // so we wrap the children in a Suspense boundary until it's ready.
   return (
     <Suspense fallback={null}>
       <MSWProviderWrapper>{children}</MSWProviderWrapper>
@@ -39,8 +38,8 @@ export function MSWProvider({
 }
 
 function MSWProviderWrapper({
-                              children,
-                            }: Readonly<{
+  children,
+}: Readonly<{
   children: React.ReactNode
 }>) {
   use(mockingEnabledPromise)
